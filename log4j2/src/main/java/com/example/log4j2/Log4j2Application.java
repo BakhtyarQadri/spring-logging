018d308c759b00c1b2b38859fe7dc51848f7362f
bakhtyar <muhammad.bakhtyar@intigral.net>
2025-06-06 15:52:21 +0500

    update code

--- log4j2/src/main/java/com/example/log4j2/Log4j2Application.java
+++ log4j2/src/main/java/com/example/log4j2/Log4j2Application.java
@@ -1,13 +1,19 @@
 package com.example.log4j2;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Log4j2Application {
 
+	private static final Logger log = LogManager.getLogger(Log4j2Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Log4j2Application.class, args);
+		System.out.println("welcome to the log4j2 world");
+		log.info("welcome to the log4j2 world");
 	}
 
 }
