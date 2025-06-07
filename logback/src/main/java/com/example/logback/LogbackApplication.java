0e4237b1310d8c241447425a3d48a6b0919b612f
bakhtyar <muhammad.bakhtyar@intigral.net>
2025-06-07 16:37:41 +0500

    update code

--- logback/src/main/java/com/example/logback/LogbackApplication.java
+++ logback/src/main/java/com/example/logback/LogbackApplication.java
@@ -1,13 +1,18 @@
 package com.example.logback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogbackApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(LogbackApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(LogbackApplication.class, args);
+        log.info("welcome to the Logback world");
     }
 
 }
