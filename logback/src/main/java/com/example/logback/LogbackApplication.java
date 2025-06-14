8695b37ca4f26990dfc5ae2d38aa63475843ca67
bakhtyar <muhammad.bakhtyar@intigral.net>
2025-06-15 00:20:33 +0500

    practice logback configs

--- logback/src/main/java/com/example/logback/LogbackApplication.java
+++ logback/src/main/java/com/example/logback/LogbackApplication.java
@@ -2,9 +2,13 @@ package com.example.logback;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class LogbackApplication {
 
@@ -18,6 +22,12 @@ public class LogbackApplication {
         log.info("welcome to the {} world", LOGGER_NAME);
         log.debug("welcome to the {} world", LOGGER_NAME);
         log.trace("welcome to the {} world", LOGGER_NAME);
+    }
+
+    @GetMapping("/")
+    public void greeting() {
+        MDC.put("user", "Bakhtyar");
+        log.info("bye bye");
     }
 
 }
