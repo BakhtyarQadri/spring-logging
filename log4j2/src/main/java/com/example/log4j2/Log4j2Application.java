4bc77bd8d8ffcc8c0a5d14ea3120896bfe9efcee
bakhtyar <muhammad.bakhtyar@intigral.net>
2025-06-22 00:01:29 +0500

    module setup

--- log4j2/src/main/java/com/example/log4j2/Log4j2Application.java
+++ log4j2/src/main/java/com/example/log4j2/Log4j2Application.java
@@ -1,19 +1,36 @@
 package com.example.log4j2;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Log4j2Application {
 
-	private static final Logger log = LogManager.getLogger(Log4j2Application.class);
+	private static final String LOGGER_NAME = "'Log4j2'";
+	private static final Logger log = LoggerFactory.getLogger(Log4j2Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Log4j2Application.class, args);
-		System.out.println("welcome to the log4j2 world");
-		log.info("welcome to the log4j2 world");
+
+		log.error("welcome to the {} world", LOGGER_NAME);
+		log.warn("welcome to the {} world", LOGGER_NAME);
+		log.info("welcome to the {} world", LOGGER_NAME);
+		log.debug("welcome to the {} world", LOGGER_NAME);
+		log.trace("welcome to the {} world", LOGGER_NAME);
+	}
+
+	@GetMapping("/")
+	public void greeting() {
+		MDC.put("user", "Bakhtyar");
+		log.info("bye bye");
+
+		MDC.clear(); // clear MDC context after use
 	}
 
 }
